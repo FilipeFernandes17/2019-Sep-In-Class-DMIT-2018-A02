003a5351b5013c9df081a1c2bcf51de542eb4820
FilipeFernandes17 <43007033+FilipeFernandes17@users.noreply.github.com>
2019-10-01 11:41:01 -0600

    Update

---
+++ src/West Wind Demo/WestWindSystem/DataModels/SupplierProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestWindSystem.DataModels
+{
+    public class SupplierProduct
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Price { get; set; }
+        public string QtyPerUnit { get; set; }
+    }
+
+}
