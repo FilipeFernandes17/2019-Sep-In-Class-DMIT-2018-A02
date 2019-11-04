7ff0bf717804b4ff89e2f0cbf5642a79165fabf5
FilipeFernandes17 <43007033+FilipeFernandes17@users.noreply.github.com>
2019-11-04 15:30:13 -0700

    Update

---
+++ src/Custom Identity/WebApp/Admin/Security/Secuirty.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Admin.Security
+{
+    /*<summary>
+    A collection of application-wide settings that provide values for secuirity roles which have been set in the web.config's
+    <appSettings /> section.
+    </summary>   
+    */
+    public class Secuirty
+    {
+        public static string AdminRole => ConfigurationManager.AppSettings["adminRole"];
+        /*
+         The above is the same as typing:
+         public static string adminrole
+         {
+            get{return ConfigurationManager.AppSettings["adminRole"];
+         }
+         */
+        public static string UserRole => ConfigurationManager.AppSettings["userRole"];
+    }
+}
